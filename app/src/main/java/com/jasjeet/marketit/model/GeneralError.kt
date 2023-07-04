1a42c28a9dcb0e010ce06ecd8f26d71807a0d398
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2023-07-04 16:45:58 +0530

    Documentation

--- app/src/main/java/com/jasjeet/marketit/model/GeneralError.kt
+++ app/src/main/java/com/jasjeet/marketit/model/GeneralError.kt
@@ -1,10 +1,7 @@
 package com.jasjeet.marketit.model
 
+/** Errors which are common to one or more API endpoints of different repositories belong here.*/
 enum class GeneralError(override val genericToast: String, override var actualResponse: String? = null): ResponseError {
-    
-    AUTH_HEADER_NOT_FOUND(genericToast = "Please login in order to perform this operation."),
-    
-    RATE_LIMIT_EXCEEDED(genericToast = "Server slow down detected."),
     
     NETWORK_ERROR(genericToast = "Please check you internet connection."),
     
